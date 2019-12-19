c22d034f50b9f9bfe6a673c574ca92c583ce5d4b
toggame <toggame@163.com>
2019-12-19 16:37:50 +0800

    逻辑运算符及三目运算符

---
+++ logic_operator_test.py
@@ -0,0 +1,20 @@
+print(not False)  # True
+print(5 > 3 and 20.0 > 10)  # True
+print(5 > 3 and 20.0 > 10)  # True
+print(4 > 5 or "c" > "a")  # True
+
+bookName = '学习Python'
+price = 79
+version = '正式版'
+if bookName.endswith('Python') and (price < 50 or version == '正式版'):
+    print('打算购买')
+else:
+    print('不购买')
+
+a = 5
+b = 8
+st = "a大于b" if a > b else "a不大于b"
+print(st)
+print("a大于b") if a > b else print("a不大于b")
+c = 5
+print('c大于a') if c > a else print('c小于a') if c < a else print('c等于a')
